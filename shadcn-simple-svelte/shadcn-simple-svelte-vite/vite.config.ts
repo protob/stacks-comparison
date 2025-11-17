690dc32a90234fbb86032029483c4fd8dd8993eb
protob <protobx@gmail.com>
2025-11-17 01:24:25 +0100

    add shadcdn simple svelte vite

---
+++ shadcn-simple-svelte/shadcn-simple-svelte-vite/vite.config.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [tailwindcss(), svelte()], resolve: {
+        alias: {
+            $lib: path.resolve("./src/lib"),
+        },
+    },
+});
